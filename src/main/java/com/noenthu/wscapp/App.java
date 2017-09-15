adc8559d841f5e6b76479a90ae1e519ee4b94fc2
Vivek Chowdhary <noenthu@gmail.com>
2017-09-14 22:11:20 -0500

    updated with test code

--- src/main/java/com/noenthu/wscapp/App.java
+++ src/main/java/com/noenthu/wscapp/App.java
@@ -1,5 +1,10 @@
 package com.noenthu.wscapp;
 
+import com.sforce.soap.partner.*;
+import com.sforce.soap.partner.sobject.*;
+import com.sforce.ws.*;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +13,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            ConnectorConfig config = new ConnectorConfig();
+
+            config.setUsername("yourusernamehere");
+            config.setPassword("yourpasswordhere");
+
+            PartnerConnection connection = Connector.newConnection(config);
+            SObject account = new SObject();
+            account.setType("Account");
+            account.setField("Name", "My Test Account from Java");
+            connection.create(new SObject[]{account});
+
+        } catch (ConnectionException el) {
+            el.printStackTrace();
+        }
+
+
     }
 }
